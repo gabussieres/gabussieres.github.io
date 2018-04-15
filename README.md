8c4c977a2fe57a07746b9e0f88b7e63d853a4c2c
Gabriel <gab.bussieres@gmail.com>
2018-04-15 13:06:20 -0400

    Destroy old website - New website under construction

---
+++ README.md
@@ -0,0 +1,9 @@
+This is my personal website, made with [create-react-app](https://github.com/facebook/create-react-app)
+
+### Roadmap
+
+* [syled-components](https://www.styled-components.com/)
+* [Prettier](https://prettier.io/)
+* Update and add resume
+* Create favicon.ico
+* Switch to TypeScript
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Gabriel Bussieres</h1>
+        </header>
+        <p>
+          Hi, I'm Gab. My page is going through a major revamp due to the dev
+          skills I acquired over the last two years. A new UI is coming soon!
+        </p>
+        <p>
+          I am a Software Engineer in Test on the iOS team at{" "}
+          <a href="https://flipp.com/" target="_blank">
+            Flipp
+          </a>
+        </p>
+        <p>
+          I studied computer science and psychology at{" "}
+          <a href="https://www.utoronto.ca/" target="_blank">
+            University of Toronto
+          </a>
+        </p>
+        <p>
+          My interests include front-end development, human-computer
+          interaction, programming languages, and cognitive psychology.
+        </p>
+        <a href="https://github.com/gabussieres" target="_blank">
+          GitHub
+        </a>
+        <br />
+        <a href="https://www.linkedin.com/in/gabrielbussieres" target="_blank">
+          LinkedIn
+        </a>
+        <p />
+      </div>
+    );
+  }
+}
+
+export default App;
